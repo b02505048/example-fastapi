d6f8d20cd44b44e04167caa961e74c5d40471608
Vann Goh <y_goh@eaglys.co.jp>
2023-01-25 12:43:41 +0900

    Changed from flatten to nested
    - Removed the start.sh (unnecessary in Azure App Service, please use the Startup Command)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Request, HTTPException
-from .routers import server_info
+from app.routers import server_info
 
 server = FastAPI()
 server.include_router(server_info.router)
-
